2cd5ccadd2016bd2a782301206c8e6743a6fb8f8
Swapnil Mahajan <swapnilsm@gmail.com>
2017-09-01 00:14:19 +0530

    Fixed packaging issue

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import io
-from glob import glob
-from os.path import basename
 from os.path import dirname
 from os.path import join
-from os.path import splitext
 
 from setuptools import setup
 
@@ -22,14 +19,14 @@ def read(*names, **kwargs):
 
 setup(
     name='redis-rw-lock',
-    version='1.0.1',
+    version='1.0.3',
     license='MIT',
     description="Redis based Reader-Writer lock with Writer's priority.",
     long_description='',
     author='Swapnil S. Mahajan',
     author_email='swapnilsm@gmail.com',
     url='https://github.com/swapnilsm/redis-rw-lock',
-    py_modules=[splitext(basename(path))[0] for path in glob('redis_rw_lock/*.py')],
+    packages=['redis_rw_lock', ],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
